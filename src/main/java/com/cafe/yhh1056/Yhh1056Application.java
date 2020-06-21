5a62c5f866a084bd8e9b2999e538840b041e2237
yhh1056 <yhh1056@naver.com>
2020-06-21 15:30:30 +0900

    init project

---
+++ src/main/java/com/cafe/yhh1056/Yhh1056Application.java
@@ -0,0 +1,13 @@
+package com.cafe.yhh1056;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yhh1056Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yhh1056Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cafe/yhh1056/Yhh1056ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.yhh1056;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yhh1056ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
