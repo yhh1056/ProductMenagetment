e8e99ed7001723af6e7b6e54e15f9faaba21091f
yhh1056 <yhh1056@naver.com>
2020-06-24 15:12:41 +0900

    Impl Password Encoder
    비밀번호 인코딩 기능 추가

--- product-admin-api/src/main/java/com/cafe/yhh1056/service/UserService.java
+++ product-admin-api/src/main/java/com/cafe/yhh1056/service/UserService.java
@@ -4,6 +4,8 @@ import com.cafe.yhh1056.domain.User;
 import com.cafe.yhh1056.domain.exception.EmailExistedException;
 import com.cafe.yhh1056.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -22,10 +24,16 @@ import java.util.Optional;
 public class UserService {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     public List<User> getAllUsers() {
@@ -33,15 +41,14 @@ public class UserService {
     }
 
     public User memberRegister(String name, String email, String password) {
-        /**
-         * 가입부분 이미 존재하는 이메일이 있을경우 예외처
-         */
         Optional<User> overlap = userRepository.findByEmail(email);
         if(overlap.isPresent()) {
             throw new EmailExistedException(email);
         }
 
-        User user = User.builder().name(name).email(email).password(password).build();
+        String encodePassword = passwordEncoder().encode(password);
+
+        User user = User.builder().name(name).email(email).password(encodePassword).build();
 
         return userRepository.save(user);
     }
